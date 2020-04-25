262ee3ca2fb924b05eacc4de9239413de2f39b73
Gabriele D'Arrigo <darrigo.g@gmail.com>
2020-04-25 17:02:49 +0200

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,54 +1,53 @@
 /*
- * MonsterId for NodeJs, version 0.1.0
+ * MonsterId for NodeJs, version 1.0.0
  * Author Gabriele D'Arrigo - @acirdesign
  * Original php library by Andreas Gohr - http://www.splitbrain.org/
 */
 import gd from 'node-gd';
 import seed from 'seed-random';
 import crypto from 'crypto';
-import randomNumber from './utils/random-number.js';
+import randomNumber from './utils/random-number';
 
-const size = 120;
+const DEFAULT_SIZE = 120;
 
 export default {
-	async fillParts(avatar, size = 120, getRandomNumber, gd) {
-		const parts = {
-			legs: getRandomNumber(5, 1),
-			hair: getRandomNumber(5, 1),
-			arms: getRandomNumber(5, 1),
-			body: getRandomNumber(15, 1),
-			eyes: getRandomNumber(15, 1),
-			mouth: getRandomNumber(10, 1)
-		};
+  async fillParts(avatar, size = 120, getRandomNumber) {
+    const parts = {
+      legs: getRandomNumber(5, 1),
+      hair: getRandomNumber(5, 1),
+      arms: getRandomNumber(5, 1),
+      body: getRandomNumber(15, 1),
+      eyes: getRandomNumber(15, 1),
+      mouth: getRandomNumber(10, 1),
+    };
 
-		for (let part in parts) {
-			let path = __dirname + '/../images/parts/' + part + '_' + parts[part] + '.png';
-			let image = await gd.createFromPng(path);
+    Object.keys(parts).forEach(async (part) => {
+      const path = `${__dirname}/../images/parts/${part}_${parts[part]}.png`;
+      const image = await gd.createFromPng(path);
+      image.copy(avatar, 0, 0, 0, 0, size, size);
 
-			image.copy(avatar, 0, 0, 0, 0, size, size);
+      if (part === 'body') {
+        avatar.fill(0, 0, avatar.colorAllocate(
+          getRandomNumber(200, 55),
+          getRandomNumber(200, 55),
+          getRandomNumber(200, 55),
+        ));
+      }
+    });
 
-			if (part === 'body') {
-				avatar.fill(0, 0, avatar.colorAllocate(
-					getRandomNumber(200, 55),
-					getRandomNumber(200, 55),
-					getRandomNumber(200, 55)
-				));
-			};
-		}
+    // Return the avatar.
+    return avatar.pngPtr();
+  },
+  async getAvatar(username = '') {
+    const hash = crypto.createHash('md5').update(username).digest('hex');
+    const rseed = parseInt(hash.substr(0, 6), 16);
+    const getRandomNumber = randomNumber(seed(rseed)());
 
-		// Return the avatar.
-		return avatar.pngPtr();
-	},
-	async getAvatar(username = '') {
-		const hash = crypto.createHash('md5').update(username).digest('hex');
-		const rseed = parseInt(hash.substr(0, 6), 16);
-		const getRandomNumber = randomNumber(seed(rseed)());
+    // Create the image with gd.
+    const image = await gd.createTrueColorSync(DEFAULT_SIZE, DEFAULT_SIZE);
+    const color = image.colorAllocate(255, 255, 255);
+    image.fill(0, 0, color);
 
-		// Create the image with gd.
-		let image = await gd.createTrueColorSync(size, size);
-		let color = image.colorAllocate(255, 255, 255);
-		image.fill(0, 0, color);
-
-		return this.fillParts(image, size, getRandomNumber, gd);
-	}
-}
+    return this.fillParts(image, DEFAULT_SIZE, getRandomNumber);
+  },
+};
--- src/utils/random-number.js
+++ src/utils/random-number.js
@@ -1,7 +1,7 @@
 export default function randomNumber(constant) {
-	const c = constant;
+  const c = constant;
 
-	return function(index = 1, add = 1) {
-		return Math.floor(c * index) + add;
-	}
+  return function rand(index = 1, add = 1) {
+    return Math.floor(c * index) + add;
+  };
 }
