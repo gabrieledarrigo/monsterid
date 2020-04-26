29caf12e5cc8f57fef360aefb44164e298571f95
Gabriele D'Arrigo <darrigo.g@gmail.com>
2020-04-26 19:40:56 +0200

    Export monsterId for CommonJS
    - Refactor random-number functions
    - Added tests to random-number function
    - Refactor monsterId function

--- src/index.js
+++ src/index.js
@@ -3,51 +3,6 @@
  * Author Gabriele D'Arrigo - @acirdesign
  * Original php library by Andreas Gohr - http://www.splitbrain.org/
 */
-import gd from 'node-gd';
-import seed from 'seed-random';
-import crypto from 'crypto';
-import randomNumber from './utils/random-number';
+import monsterId from './monsterId';
 
-const DEFAULT_SIZE = 120;
-
-export default {
-  async fillParts(avatar, size = 120, getRandomNumber) {
-    const parts = {
-      legs: getRandomNumber(5, 1),
-      hair: getRandomNumber(5, 1),
-      arms: getRandomNumber(5, 1),
-      body: getRandomNumber(15, 1),
-      eyes: getRandomNumber(15, 1),
-      mouth: getRandomNumber(10, 1),
-    };
-
-    Object.keys(parts).forEach(async (part) => {
-      const path = `${__dirname}/../images/parts/${part}_${parts[part]}.png`;
-      const image = await gd.createFromPng(path);
-      image.copy(avatar, 0, 0, 0, 0, size, size);
-
-      if (part === 'body') {
-        avatar.fill(0, 0, avatar.colorAllocate(
-          getRandomNumber(200, 55),
-          getRandomNumber(200, 55),
-          getRandomNumber(200, 55),
-        ));
-      }
-    });
-
-    // Return the avatar.
-    return avatar.pngPtr();
-  },
-  async getAvatar(username = '') {
-    const hash = crypto.createHash('md5').update(username).digest('hex');
-    const rseed = parseInt(hash.substr(0, 6), 16);
-    const getRandomNumber = randomNumber(seed(rseed)());
-
-    // Create the image with gd.
-    const image = await gd.createTrueColorSync(DEFAULT_SIZE, DEFAULT_SIZE);
-    const color = image.colorAllocate(255, 255, 255);
-    image.fill(0, 0, color);
-
-    return this.fillParts(image, DEFAULT_SIZE, getRandomNumber);
-  },
-};
+module.exports = monsterId;
--- tests/monsterid.test.js
+++ tests/monsterid.test.js
@@ -1,68 +1,45 @@
-import crypto from 'crypto';
 import sinon from 'sinon';
 import gd from 'node-gd';
 import assert from 'assert';
-import monsterId from '../src/index';
+import monsterId from '../src/monsterId';
 
-let imageMock;
 let copyFn;
 let fillFn;
 let colorAllocateFn;
+let createTrueColorFn;
+let createFromPngFn;
 
-beforeEach(() => {
-  imageMock = {
-    fill: () => {},
-    colorAllocate: () => {},
-    copy: () => {},
-    pngPtr: () => {},
-  };
+const imageMock = {
+  fill: () => {},
+  colorAllocate: () => {},
+  copy: () => {},
+  pngPtr: () => {},
+};
 
-  copyFn = sinon.spy(imageMock, 'copy');
-  fillFn = sinon.spy(imageMock, 'fill');
-  colorAllocateFn = sinon.spy(imageMock, 'colorAllocate');
-});
+describe('monsterId', () => {
+  beforeEach(() => {
+    copyFn = sinon.spy(imageMock, 'copy');
+    fillFn = sinon.spy(imageMock, 'fill');
+    colorAllocateFn = sinon.spy(imageMock, 'colorAllocate');
+    createTrueColorFn = sinon.stub(gd, 'createTrueColor').callsFake(() => Promise.resolve(imageMock));
+    createFromPngFn = sinon.stub(gd, 'createFromPng').callsFake(() => Promise.resolve(imageMock));
+  });
 
-afterEach(() => {
-  sinon.restore();
-});
+  afterEach(() => {
+    sinon.restore();
+  });
 
-test('it should create an image object ready to be filled with the various monster\' parts', async () => {
-  const username = 'username';
-  const hashMock = {
-    update: () => {},
-    digest: () => {},
-  };
+  test('it should create an image object ready to be filled with the various monster\' parts', async () => {
+    const username = 'username';
+    await monsterId(username);
 
-  const createHashFn = sinon.stub(crypto, 'createHash').callsFake(() => hashMock);
-  const updateFn = sinon.stub(hashMock, 'update').callsFake(() => hashMock);
-  const digestFn = sinon.stub(hashMock, 'digest').callsFake(() => '6761627269656c65207761732068657265');
-  const createTrueColorFn = sinon.stub(gd, 'createTrueColorSync').callsFake(() => imageMock);
-  const fillPartsFn = sinon.stub(monsterId, 'fillParts');
+    assert.equal(createTrueColorFn.calledOnce, true, 'Create a true color image');
+    assert.equal(colorAllocateFn.firstCall.calledWith(255, 255, 255), 1, 'Create the white background');
+    assert.equal(colorAllocateFn.secondCall.calledWith(sinon.match.number, sinon.match.number, sinon.match.number), 1, 'Fill with random color');
+    assert.equal(fillFn.calledTwice, true, 'Fill the avatar\'s background and the monster part');
 
-  // Create the avatar
-  await monsterId.getAvatar(username);
-
-  assert.equal(createHashFn.calledWith('md5'), true, 'Create an md5 hash');
-  assert.equal(updateFn.calledWith(username), true, 'Update the hash object');
-  assert.equal(digestFn.calledWith('hex'), true, 'Create the hexadecimal hash');
-
-  assert.equal(createTrueColorFn.callCount, 1, 'Create a true color image');
-  assert.equal(colorAllocateFn.callCount, 1, 'Create the white background');
-  assert.equal(fillFn.callCount, 1, 'Fill the avatar\'s background ');
-  assert.equal(fillPartsFn.callCount, 1, 'Fill the avatar with monster\'s body parts');
-});
-
-test('it should fill the avatar with the various monster\' parts', async () => {
-  const size = 120;
-  const createFromPngFn = sinon.stub(gd, 'createFromPng').callsFake(() => imageMock);
-  const getRandomNumberFn = sinon.spy(() => {});
-
-  await monsterId.fillParts(imageMock, size, getRandomNumberFn, gd);
-
-  assert.equal(getRandomNumberFn.callCount, 9, 'Call getRandomNumber to retrieve a random number from a seed');
-  assert.equal(createFromPngFn.callCount, 6, 'Create the image from the monster\'s parts');
-  assert.equal(copyFn.calledWith(imageMock, 0, 0, 0, 0, size, size), true, 'Copy the new image with the choosen monster\'s part');
-
-  assert.equal(colorAllocateFn.callCount, 1, 'Create a random color for the choosen part');
-  assert.equal(fillFn.callCount, 1, 'Fill the avatar with the random color');
+    assert.equal(createFromPngFn.callCount, 6, 'Create the image from the monster\'s parts');
+    assert.equal(copyFn.calledWith(imageMock, 0, 0, 0, 0, 120, 120), true, 'Copy the new image with the choosen monster\'s part');
+    assert.equal(colorAllocateFn.secondCall.calledWith(sinon.match.number, sinon.match.number, sinon.match.number), true, 'Fill with random color');
+  });
 });
